4ef6a15f3bc8070f94ad71fc581b7b6134072d40
Hrittik Roy <67012359+hrittikhere@users.noreply.github.com>
2022-05-20 07:58:05 +0000

    Add Unmarshall logic

---
+++ main.go
@@ -0,0 +1,62 @@
+package main 
+
+import (
+	"fmt"
+	"gopkg.in/yaml.v3"
+	"io/ioutil"
+)
+
+
+// Subcategories
+type Subcategories struct {
+	Name        string      `yaml:"name"`
+	Items       []Items     `yaml:"items"`
+	Subcategory interface{} `yaml:"subcategory"`
+}
+
+// Items
+type Items struct {
+	Item        interface{} `yaml:"item"`
+	Name        string      `yaml:"name"`
+	HomepageUrl string      `yaml:"homepage_url"`
+	RepoUrl     string      `yaml:"repo_url"`
+	Logo        string      `yaml:"logo"`
+	Twitter     string      `yaml:"twitter"`
+	Crunchbase  string      `yaml:"crunchbase"`
+}
+
+// LandscapeSchema
+type LandscapeSchema struct {
+	Landscape []Landscape `yaml:"landscape"`
+}
+
+// Landscape
+type Landscape struct {
+	Subcategories []Subcategories `yaml:"subcategories"`
+	Category      interface{}     `yaml:"category"`
+	Name          string          `yaml:"name"`
+}
+
+
+func main(){
+
+	landscape, err := ioutil.ReadFile("landscape.yml")
+	if err != nil {
+		fmt.Println("Error reading config file: ", err)
+	}
+
+	var landscapeconfig LandscapeSchema 
+
+	err = yaml.Unmarshal(landscape, &landscapeconfig)
+	if err != nil {
+		fmt.Println("Error parsing config file: ", err)
+	}
+
+	for _, feed := range landscapeconfig.Landscape {	
+			newfeed:=feed.Subcategories
+			
+
+			fmt.Print(nesfeed)
+	}
+
+}
